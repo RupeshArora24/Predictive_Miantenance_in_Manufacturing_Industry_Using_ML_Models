f49e0d8ab1cf8e3484eebe85e97858b35f24a39c
RupeshArora24 <rupesh.arora303@gmail.com>
2023-11-27 14:48:01 +0530

    shifted the dxc coding questions folder to the 7th semester folder

--- DXC Coding Questions Practice/even_odd.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input())
-if num1%2 == 0:
-    print("Even Number")
-else:
-    print("Odd Number")    
--- DXC Coding Questions Practice/palindrome.py
+++
@@ -1,6 +0,0 @@
-num1 = input()
-temp = num1[::-1]
-if num1==temp:
-    print("The entered number is palindrome")
-else:
-    print("The entered number is not palindrome")    
